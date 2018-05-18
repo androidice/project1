9d68406fc437c64411427384b8fb06f6f8f063ae
Kevin.Alviola <Kevin.Alviola@practicemax.com>
2018-05-18 14:08:32 +0800

    adding auth module, with angular forms, router module and reactive forms

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -4,6 +4,7 @@
 
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router} from '@angular/router';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-auth-page',
@@ -11,13 +12,35 @@ import { ActivatedRoute, Router} from '@angular/router';
     
 })
 export class AuthComponent implements OnInit {
+    authType: String = '';
     title: String = '';
+    authForm : FormGroup;
+    isSubmitting = false;
     
-    constructor() {
-
+    constructor(
+        private route: ActivatedRoute,
+        private fb: FormBuilder
+    ) {
+        this.authForm = this.fb.group({
+            'email': ['', Validators.required],
+            'password': ['', Validators.required]
+        });
     }
 
     ngOnInit(){
-        this.title = "Auth Page";
+        this.route.url.subscribe(data => {
+            this.authType = data[data.length -1].path;  
+            this.title = (this.authType === 'login')? 'Sign in': 'Sign up';
+
+            if(this.authType === 'register'){
+                this.authForm.addControl('username', new FormControl());
+            }
+        });
+    }
+
+    submitForm(){
+        const credentials = this.authForm.value;
+        console.log('credentials', credentials);
+        console.log('submitting form');
     }
 }
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 
 import { AuthComponent } from './auth.component';
+import { SharedModule } from '../shared/shared.module';
 import { AuthRoutingModule } from './auth-routing.module';
 
 @NgModule({
     imports: [
-        AuthRoutingModule
+        AuthRoutingModule,
+        SharedModule
     ],
     declarations: [
         AuthComponent
